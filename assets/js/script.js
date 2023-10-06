efc0eed035b0a13ad250bb76f2ac401af22cefe2
nkartikaxioned <kartikn@axioned.com>
2023-10-06 13:51:49 +0530

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
 /* Author: 
 
 */
+const output = document.querySelector('.result'),
+  checkbox = document.querySelectorAll('.checkbox'),
+  passLength = document.querySelector('.password_length'),
+  passUpperCase = document.querySelector('.uppercase'),
+  passLowerCase = document.querySelector('.lowercase'),
+  passNumeric = document.querySelector('.number'),
+  passSymbol = document.querySelector('.symbol'),
+  copyButton = document.querySelector('.copy_button'),
+  generatePassBtn = document.querySelector('.generate_pass_btn');
 
+
+window.addEventListener('load', () => {
+
+  generatePassBtn.addEventListener('click', () => {
+
+    checkbox.forEach((check, index) => {
+      if (check.checked == true && index!==1 && index!==2 && index!==3) {
+        console.log(index);
+      }
+    })
+  })
+})
 
 
 
